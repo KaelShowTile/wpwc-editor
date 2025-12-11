f0815189f682a80f5c2a59d9a65fc55b26dcbfed
KaelShowTile <kaelshowtile@gmail.com>
2025-12-12 10:41:02 +1100

    2025121201
    fix product attribute bug

--- includes/process_update_queue.php
+++ includes/process_update_queue.php
@@ -88,11 +88,87 @@ try {
 
             case 'attribute':
                 // Product attributes
-                if (isset($request_data['taxonomy'])) {
-                    $taxonomy = $request_data['taxonomy'];
-                    wp_set_object_terms($product_id, $new_value ? explode(', ', $new_value) : [], $taxonomy);
+                $taxonomy = $request_data['taxonomy'];
+                if ($taxonomy) {
+                    $product = wc_get_product($product_id);
+                    if (!$product) {
+                        throw new Exception('Product not found');
+                    }
+
+                    // Ensure the attribute taxonomy exists
+                    $taxonomy_id = wc_attribute_taxonomy_id_by_name($taxonomy);
+                    if (!$taxonomy_id) {
+                        // Create the attribute taxonomy
+                        $attribute_name = substr($taxonomy, 3); // Remove 'pa_' prefix
+                        $new_attr = wc_create_attribute(array(
+                            'name' => $attribute_name,
+                            'slug' => $attribute_name,
+                            'type' => 'select',
+                            'order_by' => 'menu_order',
+                            'has_archives' => 0
+                        ));
+                        if (is_wp_error($new_attr)) {
+                            throw new Exception('Failed to create attribute taxonomy: ' . $new_attr->get_error_message());
+                        }
+                        $taxonomy_id = $new_attr;
+                    }
+
+                    // Get existing product attributes
+                    $product_attributes = $product->get_attributes();
+
+                    // Check if attribute exists on product
+                    $attribute_exists = isset($product_attributes[$taxonomy]);
+
+                    if (!$attribute_exists) {
+                        // Create new attribute configuration
+                        $attribute = new WC_Product_Attribute();
+
+                        // CRITICAL: Set taxonomy flag correctly
+                        $attribute->set_id($taxonomy_id);
+                        $attribute->set_name($taxonomy);
+                        $attribute->set_visible(true);
+                        $attribute->set_variation(false);
+
+                        // Set is_taxonomy flag through the options array
+                        $options = $attribute->get_options();
+                        $options['is_taxonomy'] = 1; // 1 = true for taxonomy attributes
+
+                        // For taxonomy attributes, we don't store values in options
+                        $options['value'] = '';
+                        $attribute->set_options($options);
+
+                        $product_attributes[$taxonomy] = $attribute;
+                    }
+
+                    // Process terms
+                    $terms = !empty($new_value) ? array_map('trim', explode(',', $new_value)) : [];
+
+                    // Create terms and get term IDs
+                    $term_ids = [];
+                    foreach ($terms as $term_name) {
+                        if (!term_exists($term_name, $taxonomy)) {
+                            $new_term = wp_insert_term($term_name, $taxonomy);
+                            if (!is_wp_error($new_term)) {
+                                $term_ids[] = $new_term['term_id'];
+                            }
+                        } else {
+                            $term = get_term_by('name', $term_name, $taxonomy);
+                            $term_ids[] = $term->term_id;
+                        }
+                    }
+
+                    // Set terms to product
+                    wp_set_object_terms($product_id, $term_ids, $taxonomy, false);
+
+                    // Update product attributes
+                    $product->set_attributes($product_attributes);
+                    $product->save();
+                    
+                    // Clear product cache
+                    //wc_delete_product_transients($product_id);
+                    clean_post_cache($product_id);
+                    $success = true;
                 }
-                $success = true;
                 break;
 
             case 'glint_qty_step':
